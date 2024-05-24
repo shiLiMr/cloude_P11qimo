d795eac1cb860fab91af2d625d8e231221641069
shilihaoa <lhxhl0303.163.com>
2024-05-24 08:54:25 +0800

    init

---
+++ demo/src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path:'/',
+      name:'home',
+      component:()=>import('@/layout/index.vue')
+    },
+    {
+      path:"/login",
+      name:'login',
+      component:()=>import('@/views/login/login.vue')
+    }
+  ]
+})
+
+export default router
---
+++ demo/src/views/login/login.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="login">
+    <div class="login-main">
+        
+    </div>
+</div>
+</template>
+<script setup lang='ts'>
+
+
+</script>
+<style lang='scss' scoped>
+
+</style>
